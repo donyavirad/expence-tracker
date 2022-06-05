5fba1994abbac4fa23f69e218cb182bb4396e1df
hamed <hameddonyavi81@gmail.com>
2022-06-05 21:07:02 +0430

    dynamic result in income component

--- src/components/Income/Income.js
+++ src/components/Income/Income.js
@@ -1,7 +1,24 @@
 import React from "react"
 import "./Income.scss"
 import Card from "../../hoc/Card"
+import { useSelector } from "react-redux"
 const Income = () =>{
+    const data = useSelector((state)=> state.data)
+
+    const income = ()=>{
+        let incomes = []
+        for(let item in data){
+            if(data[item].status === "income"){
+                const num = Number(data[item].price)
+                incomes.push(num)
+            }
+        }
+        const res = incomes.reduce((a,b)=>{
+            return a + b
+        }, 0)
+        
+        return res
+    }
     return (
         <div className="income">
             <Card>
@@ -10,7 +27,7 @@ const Income = () =>{
                         درآمد:
                     </h3>
                     <span>
-                        230 هزار تومان
+                        {income()} هزار تومان
                     </span>
                 </div>
             </Card>
