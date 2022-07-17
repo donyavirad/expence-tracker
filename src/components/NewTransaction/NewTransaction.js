9788e72c66bd19bc7ce7f68e06b2d04bb01a0ead
hamed <hameddonyavi81@gmail.com>
2022-07-17 11:16:06 +0430

    add input component and add dark mode to newTransaction component

--- src/components/NewTransaction/NewTransaction.js
+++ src/components/NewTransaction/NewTransaction.js
@@ -1,14 +1,61 @@
 import React from "react"
 import "./NewTransaction.scss"
 import axios from "axios"
-import { useSelector } from "react-redux"
-import { useDispatch } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import Input from "../Input/Input"
 const NewTransaction = React.memo((props)=>{
     const dispatch = useDispatch()
     const desc = useSelector((state)=> state.form.desc)
     const price = useSelector((state)=> state.form.price)
     const status = useSelector((state)=> state.form.status)
     const loading = useSelector((state)=> state.form.loading)
+
+    const formData = {
+        priceInput: {
+            config: {
+                className: "form-input",
+                id: "priceInput",
+                type: "number",
+                placeholder:"مقدار پول به تومان می باشد",
+                min: 0,
+                required: true,
+            },
+            label: "مقدار پول",
+            value:''
+        },
+        descInput: {
+            config: {
+                className: "form-input",
+                id: "descInput",
+                type: "text",
+                placeholder:"توضیحات را وارد کنید.",
+                required: true,
+            },
+            label: "توضیحات ",
+            value:''
+        },
+        incomeRadioInput: {
+            config: {
+                class:"form-input-radio",
+                type: "radio",
+                id: "income",
+                name: "status-price",
+            },
+            value:"income",
+            label: "درآمد"
+        },
+        expendRadioInput: {
+            config: {
+                class:"form-input-radio",
+                type: "radio",
+                id: "expend",
+                name: "status-price",
+            },
+            value: "expend",
+            label: "خرج"
+        }
+    }
+
     const submitHandler = (e)=>{
         e.preventDefault()
         const createdAt = new Date().getTime()
@@ -32,45 +79,36 @@ const NewTransaction = React.memo((props)=>{
             <div className="form">
                 <h2 className="form-title">افزودن مقدار جدید</h2>
                 <form className="form-container" onSubmit={submitHandler}>
-                    <input 
-                        className="form-input" 
-                        type="text" 
-                        placeholder="توضیحات"
-                        value={useSelector((state)=> state.desc)}
-                        onChange={(e)=> dispatch({type: "DESCCHANGE",value: e.target.value})}
-                        required
-                    />
-                    <input 
-                        className="form-input" 
-                        type="number" 
-                        placeholder="مقدار"
+                    <Input 
+                        change={(e)=> dispatch({type: "NUMBERCHANGE",value: e.target.value})}
+                        config={formData.priceInput.config}
                         value={useSelector((state)=> state.number)}
-                        onChange={(e)=> dispatch({type: "NUMBERCHANGE",value: e.target.value})}
-                        min="0"
-                        required
+                        label={formData.priceInput.label}
+                    />
+                    <Input 
+                        change={(e)=> dispatch({type: "DESCCHANGE",value: e.target.value})}
+                        config={formData.descInput.config}
+                        value={useSelector((state)=> state.desc)}
+                        label={formData.descInput.label}
                     />
                     <div className="form-radio">
                         <div className="form-radio-button">
-                            <input
-                                type="radio"
-                                id="income"
-                                name="status-price"
-                                value="income"
-                                onChange={(e)=> dispatch({type: "INCOME" , value: e.target.value})}
+                            <Input
+                                config={formData.incomeRadioInput.config}
+                                value={formData.incomeRadioInput.value}
+                                change={(e)=> dispatch({type: "INCOME" , value: e.target.value})}
                                 checked={status === "income" ? true : false}
+                                label={formData.incomeRadioInput.label}
                             />
-                            <label htmlFor="income">درآمد</label>
                         </div>
                         <div className="form-radio-button">
-                            <input
-                                type="radio"
-                                id="expend"
-                                name="status-price"
-                                value="expend"
-                                onChange={(e)=> dispatch({type: "EXPEND" , value: e.target.value}) }
+                            <Input
+                                config={formData.expendRadioInput.config}
+                                value={formData.expendRadioInput.value}
+                                change={(e)=> dispatch({type: "EXPEND" , value: e.target.value}) }
                                 checked={status === "expend" ? true : false}
+                                label={formData.expendRadioInput.label}
                             />
-                            <label htmlFor="expend">خرج</label>
                         </div>
                     </div>
                     <button 
